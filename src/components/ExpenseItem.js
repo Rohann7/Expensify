a9f0e0f2c2905049278d4ae768b08dca4f8ad1e4
Rohann7 <rohan.bandaru2629@gmail.com>
2023-03-07 23:02:03 -0800

    Add ExpenseItem and Expenses component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
-function ExpenseItem() {
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+
+function ExpenseItem({title, amount, date}) {
   return (
-    <div>
-      <div>Date</div>
-        <div>
-          <h2>Title</h2>
-          <div>Amount</div>
+    <div className='expense-item'>
+      <div>
+        <ExpenseDate date={date}/>
+      </div>
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>${amount}</div>
         </div>
     </div>
   );
 }
 
-export default ExpenseItem;
+export default ExpenseItem; 
